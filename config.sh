72c0b0c878d1a0397c8ff3f262c0092266e581be
Jonathan Chen <jonc@chen.org.nz>
2019-03-07 19:45:23 +1300

    Fetch, unpack

--- config.sh
+++ config.sh
@@ -2,3 +2,30 @@
 # Configuration
 #
 ECLIPSE_TAG="S4_11_0_RC1"
+
+ECLIPSE_AGGR="platform/eclipse.platform.releng.aggregator"
+ECLIPSE_MODULES="
+	jdt/eclipse.jdt
+	jdt/eclipse.jdt.core
+	jdt/eclipse.jdt.core.binaries
+	jdt/eclipse.jdt.debug
+	jdt/eclipse.jdt.ui
+	pde/eclipse.pde.build
+	pde/eclipse.pde.ui
+	platform/eclipse.platform
+	platform/eclipse.platform.common
+	platform/eclipse.platform.debug
+	platform/eclipse.platform.releng
+	platform/eclipse.platform.resources
+	platform/eclipse.platform.runtime
+	platform/eclipse.platform.swt
+	platform/eclipse.platform.swt.binaries
+	platform/eclipse.platform.team
+	platform/eclipse.platform.text
+	platform/eclipse.platform.ua
+	platform/eclipse.platform.ui
+	platform/eclipse.platform.ui.tools
+	equinox/rt.equinox.binaries
+	equinox/rt.equinox.bundles
+	equinox/rt.equinox.framework
+	equinox/rt.equinox.p2"
