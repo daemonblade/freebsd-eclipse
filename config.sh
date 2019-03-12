473130eadbba586a4cea1453e405221bb569be79
Jonathan Chen <jonc@chen.org.nz>
2019-03-12 21:53:31 +1300

    - restore module list to config.sh - seed port-files

--- config.sh
+++ config.sh
@@ -4,5 +4,30 @@
 ECLIPSE_TAG="S4_11_0_RC2"
 
 ECLIPSE_TOP="platform/eclipse.platform.releng.aggregator"
+ECLIPSE_MODULES="
+	jdt/eclipse.jdt
+	jdt/eclipse.jdt.core
+	jdt/eclipse.jdt.core.binaries
+	jdt/eclipse.jdt.debug
+	jdt/eclipse.jdt.ui
+	pde/eclipse.pde.build
+	pde/eclipse.pde.ui
+	platform/eclipse.platform
+	platform/eclipse.platform.common
+	platform/eclipse.platform.debug
+	platform/eclipse.platform.releng
+	platform/eclipse.platform.resources
+	platform/eclipse.platform.runtime
+	platform/eclipse.platform.swt
+	platform/eclipse.platform.swt.binaries
+	platform/eclipse.platform.team
+	platform/eclipse.platform.text
+	platform/eclipse.platform.ua
+	platform/eclipse.platform.ui
+	platform/eclipse.platform.ui.tools
+	equinox/rt.equinox.binaries
+	equinox/rt.equinox.bundles
+	equinox/rt.equinox.framework
+	equinox/rt.equinox.p2"
 
 PATCH_DIR="java-eclipse/files"
