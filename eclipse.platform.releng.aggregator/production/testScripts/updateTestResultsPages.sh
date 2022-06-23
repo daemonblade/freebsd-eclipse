45b30aa1ed501152db5bf34ee2ee371c418d2537
Jonathan Chen <jonc@chen.org.nz>
2022-06-23 13:16:40 +1200

    Reviewed (again): production

--- eclipse.platform.releng.aggregator/production/testScripts/updateTestResultsPages.sh
+++ eclipse.platform.releng.aggregator/production/testScripts/updateTestResultsPages.sh
@@ -295,7 +295,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="R-4.20-202106111600"
+  baselineCode="R-4.23-202203080310"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
