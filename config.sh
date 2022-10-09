3371e574d139a7ff4c5d8a3605f3e276a53e2cc4
Jonathan Chen <jonc@chen.org.nz>
2022-10-10 11:17:46 +1300

    R.25, update assocate git-repos

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #
 # Configuration
 #
-ECLIPSE_TAG="R4_24"
+ECLIPSE_TAG="R4_25"
 
 ECLIPSE_TOP="eclipse-platform/eclipse.platform.releng.aggregator"
 ECLIPSE_MODULES="
@@ -15,18 +15,14 @@ ECLIPSE_MODULES="
 	eclipse-platform/eclipse.platform.common
 	eclipse-platform/eclipse.platform.debug
 	eclipse-platform/eclipse.platform.releng
-	eclipse-platform/eclipse.platform.resources
-	eclipse-platform/eclipse.platform.runtime@fd42b6e331
 	eclipse-platform/eclipse.platform.swt
 	eclipse-platform/eclipse.platform.swt.binaries
-	eclipse-platform/eclipse.platform.team
 	eclipse-platform/eclipse.platform.text
 	eclipse-platform/eclipse.platform.ua
 	eclipse-platform/eclipse.platform.ui
 	eclipse-platform/eclipse.platform.ui.tools
+	eclipse-equinox/equinox
 	eclipse-equinox/equinox.binaries:rt.equinox.binaries
-	eclipse-equinox/equinox.bundles:rt.equinox.bundles
-	eclipse-equinox/equinox.framework:rt.equinox.framework
 	eclipse-equinox/p2:rt.equinox.p2"
 
 PATCH_DIR="java-eclipse/files"
